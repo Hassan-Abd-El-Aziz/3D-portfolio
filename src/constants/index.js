a78a064e207b7ed9b7e71ed62ac94bb99699d4d8
Hassan-Abd-El-Aziz <hassanabdelaziz940@gmail.com>
2025-11-17 16:42:14 +0200

    add testimonials section

--- src/constants/index.js
+++ src/constants/index.js
@@ -204,43 +204,43 @@ const expLogos = [
 
 const testimonials = [
   {
-    name: "Esther Howard",
-    mentions: "@estherhoward",
+    name: "Ahmed Amer",
+    mentions: "@ahmedamer",
     review:
       "I can’t say enough good things about Hassan. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.",
     imgPath: "/images/client1.png",
   },
   {
-    name: "Wade Warren",
-    mentions: "@wadewarren",
+    name: "Mostafa Gallal",
+    mentions: "@mostafagallal",
     review:
       "Working with Hassan was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.",
     imgPath: "/images/client3.png",
   },
   {
-    name: "Guy Hawkins",
-    mentions: "@guyhawkins",
+    name: "Mohamed Adel",
+    mentions: "@mohamedadel",
     review:
       "Collaborating with Hassan was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Hassan's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Hassan is the ideal partner.",
     imgPath: "/images/client2.png",
   },
   {
-    name: "Marvin McKinney",
-    mentions: "@marvinmckinney",
+    name: "Amr Mido",
+    mentions: "@amrmido",
     review:
       "Hassan was a pleasure to work with. He turned our outdated website into a fresh, intuitive platform that’s both modern and easy to navigate. Fantastic work overall.",
     imgPath: "/images/client5.png",
   },
   {
-    name: "Floyd Miles",
-    mentions: "@floydmiles",
+    name: "Hamada Amin",
+    mentions: "@hamadaamin",
     review:
       "Hassan’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional!",
     imgPath: "/images/client4.png",
   },
   {
-    name: "Albert Flores",
-    mentions: "@albertflores",
+    name: "Abo Ftooh",
+    mentions: "@aboftooh",
     review:
       "Hassan was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend and backend dev are top-notch.",
     imgPath: "/images/client6.png",
